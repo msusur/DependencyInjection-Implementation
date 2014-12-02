e3411d0d6e2a0cc0cf10df258dd5e8383179fc8e
msusur <mail@mertsusur.com>
2014-12-02 14:26:29 +0200

    assembly info update.

--- EasyDependencyInjection/Properties/AssemblyInfo.cs
+++ EasyDependencyInjection/Properties/AssemblyInfo.cs
@@ -1,36 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EasyDependencyInjection")]
+[assembly: AssemblyDescription("Simplest dependency injection, for small and performance agnostic projects. See https://github.com/msusur/DependencyInjection-Implementation for details and issues.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Client")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright © Client 2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("EasyDependencyInjection")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("Mert Susur")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("18915823-9545-4d3e-80f2-eb5bb9e7943e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
